63fe8b5483d16dbc29afdb0e5da9bd7a364cab71
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-16 17:01:50 +0200

    create timer.js file extract the class definition for the timer and move to new file

--- timer/index.js
+++ timer/index.js
@@ -1,67 +1,9 @@
-// create class Timer
-class Timer {
-  constructor(durationInput, startButton, pauseButton, callbacks) {
-    this.durationInput = durationInput;
-    this.startButton = startButton;
-    this.pauseButton = pauseButton;
-
-    // to say that callbacks are optionals I use if statement
-    if (callbacks) {
-      this.onStart = callbacks.onStart;
-      this.onThick = callbacks.onThick;
-      this.onComplete = callbacks.onComplete;
-    }
-
-    // addEventListener for start and pause buttons
-    this.startButton.addEventListener('click', this.start);
-    this.pauseButton.addEventListener('click', this.pause);
-  }
-  // define start method
-  start = () => {
-    // check to see if onStart callback
-    if (this.onStart) {
-      this.onStart();
-    }
-
-    this.thick();
-    this.intervalId = setInterval(this.thick, 1000);
-  };
-  // define pause method
-  pause = () => {
-    clearInterval(this.intervalId);
-  };
-  // define tick method
-  thick = () => {
-    if (this.timeRemaining <= 0) {
-      this.pause();
-      //when timer is done check if isComplete callback
-      if (this.onComplete) {
-        this.onComplete();
-      }
-    } else {
-      this.timeRemaining = this.timeRemaining - 1;
-      // check if onThick callback
-      if (this.onThick) {
-        this.onThick();
-      }
-    }
-  };
-  // define getter and setter methods to extract durationInput value of thick method
-  get timeRemaining() {
-    return parseFloat(this.durationInput.value);
-  }
-
-  set timeRemaining(time) {
-    this.durationInput.value = time;
-  }
-}
-
-//  select three different elements
+//  select  different elements
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-// create instance of the timer and pass three elements
+// create instance of the timer and pass those elements
 // add callbacks as fourth argument
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
---
+++ timer/timer.js
@@ -0,0 +1,57 @@
+// create class Timer
+class Timer {
+  constructor(durationInput, startButton, pauseButton, callbacks) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    // to say that callbacks are optionals I use if statement
+    if (callbacks) {
+      this.onStart = callbacks.onStart;
+      this.onThick = callbacks.onThick;
+      this.onComplete = callbacks.onComplete;
+    }
+
+    // addEventListener for start and pause buttons
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+  // define start method
+  start = () => {
+    // check to see if onStart callback
+    if (this.onStart) {
+      this.onStart();
+    }
+
+    this.thick();
+    this.intervalId = setInterval(this.thick, 1000);
+  };
+  // define pause method
+  pause = () => {
+    clearInterval(this.intervalId);
+  };
+  // define tick method
+  thick = () => {
+    if (this.timeRemaining <= 0) {
+      this.pause();
+      //when timer is done check if isComplete callback
+      if (this.onComplete) {
+        this.onComplete();
+      }
+    } else {
+      this.timeRemaining = this.timeRemaining - 1;
+      // check if onThick callback
+      if (this.onThick) {
+        this.onThick();
+      }
+    }
+  };
+  // define getter and setter methods to extract durationInput value of thick method
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
