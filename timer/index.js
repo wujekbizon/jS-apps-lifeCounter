f1c40be3af889ed9913f97dc44055af120b55868
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-16 17:43:25 +0200

    start work on animated border, set circle attribute

--- timer/index.js
+++ timer/index.js
@@ -2,15 +2,22 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('#circle');
+
+// set cirlce attributes
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
 // create instance of the timer and pass those elements
 // add callbacks as fourth argument
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
   onStart() {
     console.log('Timer started');
   },
   onThick() {
-    console.log('Timer just tick down');
+    circle.setAttribute('stroke-dashoffset', currentOffset);
+    currentOffset = currentOffset - 50;
   },
   onComplete() {
     console.log('Timer is completed');
