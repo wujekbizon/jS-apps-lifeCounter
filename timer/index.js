35724b0da32fd2e9e96d66138afa3fde3cf2cb2d
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2022-04-16 15:05:51 +0200

    create timer class and start method

--- timer/index.js
+++ timer/index.js
@@ -1,1 +1,22 @@
-console.log('Hi there');
+// create class Timer
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+  // define a method called start
+  start = () => {
+    console.log('Time to start the timer!');
+  };
+}
+
+//  select three different elements
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+// create instance of the timer and pass three elements
+const timer = new Timer(durationInput, startButton, pauseButton);
